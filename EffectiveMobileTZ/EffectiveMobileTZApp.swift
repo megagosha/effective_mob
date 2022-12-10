da5eb22a1c78e7a1ba0458162b4ee67375dda06b
George Tevosov <george.tevosov@fabrique.studio>
2022-12-10 17:54:53 +0400

    Initial Commit

---
+++ EffectiveMobileTZ/EffectiveMobileTZApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTZApp.swift
+//  EffectiveMobileTZ
+//
+//  Created by George Tevosov on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
